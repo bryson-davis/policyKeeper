27f77aa2de2c139e9abfe5cd9a0b99565f08a8e9
sysuKinthon <qinkai1994@qq.com>
2019-11-02 10:21:57 +0800

    完成了imagePullPolicy的限定

--- deploy-constraints.sh
+++ deploy-constraints.sh
@@ -2,13 +2,13 @@
 
 # set -x
 
-env_val_conf="value.conf"
+# env_val_conf="value.conf"
 
-while read -r line; do
-	declare "$line"
-done < ${env_val_conf}
+# while read -r line; do
+# 	declare "$line"
+# done < ${env_val_conf}
 
-#需要确保templates里面的crd对象已经生效
+# #需要确保templates里面的crd对象已经生效
 
-#部署constraints
-kubectl apply -f constraints -s ${k8s_cluster}
+# #部署constraints
+# kubectl apply -f constraints -s ${k8s_cluster}
