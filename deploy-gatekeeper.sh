7bd25316b8684538758a42e173c4aefd8672b50c
sysuKinthon <qinkai1994@qq.com>
2019-11-03 20:39:39 +0800

    修改gatekeeper部署yaml

--- deploy-gatekeeper.sh
+++ deploy-gatekeeper.sh
@@ -2,15 +2,17 @@
 
 # set -x
 
-env_val_conf="value.conf"
-outfile="gatekeeper.yaml"
-while read -r line; do
-	declare "$line"
-done < ${env_val_conf}
+# env_val_conf="value.conf"
+# outfile="gatekeeper.yaml"
+# while read -r line; do
+# 	declare "$line"
+# done < ${env_val_conf}
 
-#变量替换
-mkdir -p ./gatekeeper/out
-jinjia2 ./gatekeeper/gatekeeper-template.yaml -D constroller_manager_image=${constroller_manager_image} >./gatekeeper/out/${outfile}
+# #变量替换
+# mkdir -p ./gatekeeper/out
+# jinjia2 ./gatekeeper/gatekeeper-template.yaml -D constroller_manager_image=${constroller_manager_image} >./gatekeeper/out/${outfile}
 
-#k8s部署
-kubectl apply -f ./gatekeeper/out/${outfile} -s ${k8s_cluster}
+# #k8s部署
+# kubectl apply -f ./gatekeeper/out/${outfile} -s ${k8s_cluster}
+
+kubectl apply -f ./gatekeeper/gatekeeper.yaml
